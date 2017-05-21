54eeb5869b7a7c3f4a9c425bbf8127ff4c401b2a
robgmerrill <robgmerrill@gmail.com>
2017-05-20 17:56:44 -0700

    added es6 modules

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,20 +1,3 @@
-/* global React ReactDOM */
-
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
-// Second component
-var MyTitle = React.createClass({
-  // every component must have a render method and the method must return markup
-  // the render method must be a pure function
-  render: function () {
-    return (
-      div(null,
-        h1({style: { backgroundColor: this.props.color, fontSize: this.props.fontSize }}, this.props.title)
-      )
-    )
-  }
-})
 
 // Factories
 var MyTitleFactory = React.createFactory(MyTitle)
---
+++ js/MyTitle.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// same as "var React = require('react')"
+
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+// Second component
+var MyTitle = React.createClass({
+  // every component must have a render method and the method must return markup
+  // the render method must be a pure function
+  render: function () {
+    return (
+      div(null,
+        h1({style: { backgroundColor: this.props.color, fontSize: this.props.fontSize }}, this.props.title)
+      )
+    )
+  }
+})
+
+export default MyTitle
