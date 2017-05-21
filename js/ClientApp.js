1d1b78412b9bcd4627590009a4d9cf5251ef9138
robgmerrill <robgmerrill@gmail.com>
2017-05-20 18:23:15 -0700

    used webpack to create bundle for script source in index file

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,36 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
+
+var div = React.DOM.div
+
 
 // Factories
 var MyTitleFactory = React.createFactory(MyTitle)
 
 // parent passes on properties to children to display
-var MyFirstComponent = (
-  div(null,
-    MyTitleFactory({title: 'props are the best', color: 'peru', fontSize: '60'}),
-    MyTitleFactory({title: 'props are the second best', color: 'mediumaquamarine'}),
-    MyTitleFactory({title: 'props are the third best', color: 'tomato'}),
-    MyTitleFactory({title: 'props are the fourth best', color: 'blanchedalmond'})
+var MyFirstComponent = React.createClass({
+  render: function () {
+    return(
+    div(null,
+      MyTitleFactory({title: 'props are the best', color: 'peru', fontSize: '60'}),
+      MyTitleFactory({title: 'props are the second best', color: 'mediumaquamarine'}),
+      MyTitleFactory({title: 'props are the third best', color: 'tomato'}),
+      MyTitleFactory({title: 'props are the fourth best', color: 'blanchedalmond'})
   )
 )
+}
+})
 
-// a component is nothing but a function
-// var MyFirstComponent = (
-//   div(null,
-//     React.createElement(MyTitle),
-//     React.createElement(MyTitle),
-//     React.createElement(MyTitle),
-//     React.createElement(MyTitle)
-//   )
-// )
 
-// Long version
-// var MyFirstComponent = React.createClass({
-//   render: function() {
-//     return (
-//       React.DOM.div(null,
-//         React.DOM.h1(null, 'This is my first component!')
-//       )
-//     )
-//   }
-// })
 
-ReactDOM.render(MyFirstComponent, document.getElementById('app'))
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
