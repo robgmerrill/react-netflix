2fdcc44c6f43da1c6f086582e1e3b85ec6bb63cf
robgmerrill <robgmerrill@gmail.com>
2017-05-23 06:23:58 -0700

    added search route

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter, Match } from 'react-router'
 import Landing from './Landing'
+import Search from './Search'
 // instead of
 // import ReactDOM from 'react-dom'
 import '../public/normalize.css'
@@ -10,9 +11,15 @@ import '../public/style.css'
 const App = React.createClass({
   render () {
     return (
+      // HashRouter doesn't show you anything. it is a higher order or behavior
+      // ordered components. HashRouter is typically root component. Everything
+      // will be displayed
       <HashRouter>
         <div className='app'>
+          {/* Anytime we match '/' we will display landing component. 'exactly'
+          only catches the '/' and not '/text' and basically everything. */}
           <Match exactly pattern='/' component={Landing} />
+          <Match pattern='/search' component={Search} />
         </div>
       </HashRouter>
     )
---
+++ js/Search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Search = React.createClass({
+  render () {
+    return (
+      <h1>Search Page!!</h1>
+    )
+  }
+})
+
+export default Search
