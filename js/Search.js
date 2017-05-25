7dd288da860a1869adeb9dab5dbaec527f36ba5d
robgmerrill <robgmerrill@gmail.com>
2017-05-24 17:19:20 -0700

    displaying JSON data within Search component

--- js/Search.js
+++ js/Search.js
@@ -5,7 +5,25 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+        {/*}<pre><code>{JSON.stringify(preload, null, 4)}</code></pre>*/}
+        {/*}{preload.shows.map(function (show) {
+          return (
+            <h3>{show.title}</h3>
+          )
+        })}*/}
+        {preload.shows.map((show) => {
+          return (
+            <div className='show-card'>
+              <img src={`/public/img/posters/${show.poster}`} />
+              <div>
+                <h3>{show.title}</h3>
+                <h4>({show.year})</h4>
+                <p>{show.descriptions}</p>
+              </div>
+            </div>
+          )
+        })}
+
       </div>
     )
   }
